d1838432bcbec0219ca5f59cf579a16a9b13f2c6
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-02-15 15:28:22 -0600

    Accelerometer init

--- PhotonDemo.ino
+++ PhotonDemo.ino
@@ -1,7 +1,12 @@
 #include "Adafruit_Sensor.h"
+#include "Adafruit_MMA8451.h"
+
+#define   INTERRUPT1  2
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
 void setup() {
-
+  mma.begin(MMA8451_DEFAULT_ADDRESS);
 
 }
 
