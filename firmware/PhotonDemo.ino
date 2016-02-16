284681fb7339d1e028f9496bede96612a029315f
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-02-16 12:49:52 -0600

    Accelerometer inits properly

--- firmware/PhotonDemo.ino
+++ firmware/PhotonDemo.ino
@@ -6,7 +6,14 @@
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
 void setup() {
-  mma.begin(MMA8451_DEFAULT_ADDRESS);
+  Serial.begin(9600); // Enable serial on USB port
+  Serial.println("Hello, friend.");
+  Serial.println("Starting MMA8451");
+  if (!mma.begin(MMA8451_DEFAULT_ADDRESS)) {
+    Serial.println("MMA8451 could not initialize!");
+    while(1);       // infinite loop to "halt" the device if accelerometer fails to init
+  }
+  Serial.println("MMA8451 initialized");
 
 }
 
